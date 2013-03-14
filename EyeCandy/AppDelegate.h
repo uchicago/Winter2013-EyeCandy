dbd430348efba0d1e37d5ed3b5621d74fa485396
T. Andrew Binkowski <abinkowski@uchicago.edu>
2013-03-14 11:51:04 -0500

    Initial Commit

---
+++ EyeCandy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EyeCandy
+//
+//  Created by T. Andrew Binkowski on 3/14/13.
+//  Copyright (c) 2013 T. Andrew Binkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EyeCandy/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EyeCandy
+//
+//  Created by T. Andrew Binkowski on 3/14/13.
+//  Copyright (c) 2013 T. Andrew Binkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
